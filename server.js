98d31352c11e7d615413a8fb757c8ade4caa1495
oranblock <102727189+oranblock@users.noreply.github.com>
2024-06-15 14:40:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ wss.on('connection', (ws) => {
 
     ws.on('message', (message) => {
         console.log(`Received message: ${message}`);
+        // Broadcast the message to all connected clients
         wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message);
